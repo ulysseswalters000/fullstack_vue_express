6a2489863b6f4388d83ebbb0b81b158190907806
andrew <andrew@localhost.localdomain>
2019-01-09 12:21:32 -0500

    fixed small error

--- server/index.js
+++ server/index.js
@@ -11,9 +11,17 @@ app.use(cors());
 
 const posts = require('./routes/api/posts.js');
 
-//tells the app to use posts from ./routes/api/posts,js
+//tells the app to use posts from ./routes/api/posts.js
 //to send to the router
 app.use('/api/posts', posts);
+
+// Handle Production
+if(process.env.NODE_ENV === 'production') {
+  //static folder
+  app.use(express.static(__dirname + '/public'));
+
+  // Handle single page app
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html');
 
 const port = process.env.PORT || 5000;
 
